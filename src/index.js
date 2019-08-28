b9724f5171ac14400501285db93e0702efdc1d00
Lori Baumgartner <lori.burleson@gmail.com>
2019-08-28 12:37:16 -0400

    stub data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { WishList } from "./models/WishList";
+
+const wishList = WishList.create({
+  items: [
+    {
+      name: "New Camera",
+      price: 833.33,
+      image:
+        "https://images.unsplash.com/photo-1566864222010-d45675442c31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80"
+    },
+    {
+      name: "Circus Waffle Maker",
+      price: 35
+    }
+  ]
+});
+
+ReactDOM.render(<App wishList={wishList} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
